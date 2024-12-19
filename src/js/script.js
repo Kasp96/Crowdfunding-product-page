d5b416d8e180942b420126d826e3d15adc4f7142
Kacper <kacperwach96@gmail.com>
2024-12-19 18:21:37 +0100

    add open mobile nav feature

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-console.log('test');
+const openNavBtn = document.querySelector('.burger-btn');
+const closeNavBtn = document.querySelector('.burger-close');
+const navMenu = document.querySelector('.nav-menu');
+
+const openNavigation = () => {
+    openNavBtn.classList.toggle('hide');
+	closeNavBtn.classList.toggle('show');
+	navMenu.classList.toggle('show-mobile-nav');
+   
+};
+
+const closeNavigation = () => {
+	openNavBtn.classList.toggle('hide');
+	closeNavBtn.classList.toggle('show');
+	navMenu.classList.toggle('show-mobile-nav');
+};
+
+openNavBtn.addEventListener('click', openNavigation);
+closeNavBtn.addEventListener('click', closeNavigation);
+
