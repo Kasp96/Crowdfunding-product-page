9c2de96038ad161d4a4e1ccb15c453439b648a87
Kacper <kacperwach96@gmail.com>
2024-12-20 21:54:21 +0100

    create and style project card, add dynamic shadow to mobile nav feature

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,21 @@
 const openNavBtn = document.querySelector('.burger-btn');
 const closeNavBtn = document.querySelector('.burger-close');
 const navMenu = document.querySelector('.nav-menu');
+const navShade = document.querySelector('.nav-shade')
 
 const openNavigation = () => {
     openNavBtn.classList.toggle('hide');
 	closeNavBtn.classList.toggle('show');
 	navMenu.classList.toggle('show-mobile-nav');
-   
+	navShade.classList.toggle('show')
+	
 };
 
 const closeNavigation = () => {
 	openNavBtn.classList.toggle('hide');
 	closeNavBtn.classList.toggle('show');
 	navMenu.classList.toggle('show-mobile-nav');
+	navShade.classList.toggle('show')
 };
 
 openNavBtn.addEventListener('click', openNavigation);
